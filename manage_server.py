416a671f84b168221ac534f77932faa6eb2c7cec
IzzyWYL <55464769+IzzyWYL@users.noreply.github.com>
2019-09-18 02:56:32 +0200

    Updated: added def 'get_AuthUrl'

--- manage_server.py
+++ manage_server.py
@@ -19,11 +19,35 @@ server_ip="95.217.59.142"
 server_port=5656
 server_name="mc2.wyl-server.host"
 
+parser = ArgumentParser()
+parser.add_argument("-a", "--action", dest="action",
+                    help="Start SERVER_ACTION", metavar="SERVER_ACTION") 
+parser.add_argument("-n", "--name", dest="name",
+                    help="Name of the Server")
+
+parser.add_argument("-s", "--ssl", dest="ssl_enabled",
+                    help="Is Panel Secured?")
+   
+########################################################################################################
+   
+
+args = parser.parse_args()
+
+
+def get_proto():
+    if args.ssl_enabled == "1":
+        return "https" 
+    else:
+        return "http"
+
+proto = get_proto();
+
 def auth__oauth2():
     payload_querystr = "grant_type=client_credentials&client_id="+client_id+"&client_secret="+client_secret
     payload = urllib.parse.parse_qs(payload_querystr)
     token = False;
-    r = requests.post('http://mc2.wyl-server.host/oauth2/token/request', params=payload)
+    defaultAuthUrl = get_AuthUrl(server_name)
+    r = requests.post(defaultAuthUrl, params=payload)
     if "error" in r.text:
         print("Error: "+r.text)
         exit;
@@ -37,18 +61,18 @@ def auth__oauth2():
 def createAuthHeader(token):
     return {'Authorization': 'Bearer '+token}
 
-def get_proto():
-    if args.ssl_enabled == "1":
-        return "https" 
-    else:
-        return "http"
+
+
+def get_AuthUrl(server_name):
+    
+    return proto+"://"+server_name+'/oauth2/token/request'
 
 def gen_DefaultUrl(proto,server_name,server_port,gameserver_name):
     url = proto+"://"+server_name+":"+str(server_port)+"/server/"+gameserver_name+"/"
     return url
     
 def generate_ActionURL(token, gameserver_name, action):
-    proto = get_proto();
+    #proto = get_proto();
     defaultReqURL = (gen_DefaultUrl(proto,server_name,server_port,gameserver_name))
     actionReqURL = defaultReqURL+action
     return actionReqURL
@@ -61,21 +85,9 @@ def manage_server(token, gameserver_name, action):
 
 ########################################################################################################
 
-parser = ArgumentParser()
-parser.add_argument("-a", "--action", dest="action",
-                    help="Start SERVER_ACTION", metavar="SERVER_ACTION")
-parser.add_argument("-n", "--name", dest="name",
-                    help="Name of the Server")
-
-parser.add_argument("-s", "--ssl", dest="ssl_enabled",
-                    help="Is Panel Secured?")
-   
-########################################################################################################
-   
-
-args = parser.parse_args()
 if len(args.action) > 0:
     token = auth__oauth2()
+    print(token)
     if token is not False:
         if len(args.name) > 0:
             gameserver_name = args.name
@@ -86,9 +98,3 @@ if len(args.action) > 0:
             manage_server(token, gameserver_name, "start")
         elif args.action == "stop":
             manage_server(token, gameserver_name, "stop")
-
-
-
-
-
-
